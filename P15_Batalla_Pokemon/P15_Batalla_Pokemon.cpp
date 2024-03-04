21070c96e971cb07fa46685b35e22820742b67c0
adrian-oroanz <adrian.oroanz@gmail.com>
2024-03-04 07:34:18 -0600

    000_P15_Batalla_Pokemon
    Commit Inicial

---
+++ P15_Batalla_Pokemon/P15_Batalla_Pokemon.cpp
@@ -0,0 +1,12 @@
+// P15_Batalla_Pokemon.cpp
+// Adrian Orozco Anzures
+// Simulador de una batalla de pokemon en consola
+//
+
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
